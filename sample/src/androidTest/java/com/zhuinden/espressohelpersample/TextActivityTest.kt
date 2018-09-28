5141cc27723bcf3084823ed29bf8b2d8a3afbcd3
Ahmed Umar Bello <khattabu@gmail.com>
2018-09-28 13:07:25 +0100

    Extended tests to include visibility tests among others

--- sample/src/androidTest/java/com/zhuinden/espressohelpersample/TextActivityTest.kt
+++ sample/src/androidTest/java/com/zhuinden/espressohelpersample/TextActivityTest.kt
@@ -102,4 +102,205 @@ class TextActivityTest {
     fun hasContentDescriptionFailure(){
         verifyAssertion { R.id.iv_image_view.checkHasContentDescription("Halo") }
     }
+
+    @Test
+    fun hasHintWithId(){
+        R.id.ed_edit_text.checkHasHint(R.string.hello_world)
+    }
+
+    @Test
+    fun hasHintWithIdFailure(){
+        verifyAssertion { R.id.ed_edit_text.checkHasHint(R.string.app_name) }
+    }
+
+    @Test
+    fun hasHintWithString(){
+        R.id.ed_edit_text.checkHasHint("Hello world!")
+    }
+
+    @Test
+    fun hasHintWithStringFailure(){
+        verifyAssertion { R.id.ed_edit_text.checkHasHint("Halo") }
+    }
+
+    @Test
+    fun isChecked(){
+        R.id.cb_show_profile.checkIsChecked()
+    }
+
+    @Test
+    fun isCheckedFailure(){
+        verifyAssertion { R.id.cb_suggest.checkIsChecked() }
+    }
+
+    @Test
+    fun isNotChecked(){
+        R.id.cb_suggest.checkIsNotChecked()
+    }
+
+    @Test
+    fun isNotCheckedFailure(){
+        verifyAssertion { R.id.cb_show_profile.checkIsNotChecked() }
+    }
+
+    @Test
+    fun isVisible(){
+        R.id.iv_image_view.checkIsVisible()
+    }
+
+    @Test
+    fun isVisibleFailure(){
+        verifyAssertion { R.id.tv_empty_text_view.checkIsVisible() }
+    }
+
+    @Test
+    fun isNotVisible(){
+        R.id.tv_empty_text_view.checkIsInvisible()
+    }
+
+    @Test
+    fun isNotVisibleFailure(){
+        verifyAssertion { R.id.iv_image_view.checkIsInvisible() }
+    }
+
+    @Test
+    fun isGone(){
+        R.id.iv_rating.checkIsGone()
+    }
+
+    @Test
+    fun isGoneFailure(){
+        verifyAssertion { R.id.iv_image_view.checkIsGone() }
+    }
+
+    @Test
+    fun isFocusable(){
+        R.id.ed_edit_text.checkIsFocusable()
+    }
+
+    @Test
+    fun isFocusableFailure(){
+        verifyAssertion { R.id.tv_empty_text_view.checkIsFocusable() }
+    }
+
+    @Test
+    fun isNotFocusable(){
+        R.id.tv_empty_text_view.checkIsNotFocusable()
+    }
+
+    @Test
+    fun isNotFocusableFailure(){
+        verifyAssertion { R.id.ed_edit_text.checkIsNotFocusable() }
+    }
+
+    @Test
+    fun isFocused(){
+        R.id.ed_edit_text.checkIsFocused()
+    }
+
+    @Test
+    fun isFocusedFailure(){
+        verifyAssertion { R.id.tv_empty_text_view.checkIsFocused() }
+    }
+
+    @Test
+    fun isNotFocused(){
+        R.id.tv_empty_text_view.checkIsNotFocused()
+    }
+
+    @Test
+    fun isNotFocusedFailure(){
+        verifyAssertion { R.id.ed_edit_text.checkIsNotFocused() }
+    }
+
+    @Test
+    fun isClickable(){
+        R.id.iv_image_view.checkIsClickable()
+    }
+
+    @Test
+    fun isClickableFailure(){
+        verifyAssertion { R.id.tv_empty_text_view.checkIsClickable() }
+    }
+
+    @Test
+    fun isNotClickable(){
+        R.id.tv_empty_text_view.checkIsNotClickable()
+    }
+
+    @Test
+    fun isNotClickableFailure(){
+        verifyAssertion { R.id.iv_image_view.checkIsNotClickable() }
+    }
+
+    @Test
+    fun isEnabled(){
+        R.id.iv_image_view.checkIsEnabled()
+    }
+
+    @Test
+    fun isEnabledFailure(){
+        verifyAssertion { R.id.tv_empty_text_view.checkIsEnabled() }
+    }
+
+    @Test
+    fun isDisabled(){
+        R.id.tv_empty_text_view.checkIsDisabled()
+    }
+
+    @Test
+    fun isDisabledFailure(){
+        verifyAssertion { R.id.iv_image_view.checkIsDisabled() }
+    }
+
+    @Test
+    fun hasSpecificTag(){
+        R.id.iv_image_view.checkHasTag("Hello world!")
+    }
+
+    @Test
+    fun hasSpecificTagFailure(){
+        verifyAssertion { R.id.iv_image_view.checkHasTag("Halo") }
+    }
+
+    @Test
+    fun hasAnyTag(){
+        R.id.iv_image_view.checkHasAnyTag("Hello world!", "Halo")
+    }
+
+    @Test
+    fun hasAnyTagFailure(){
+        verifyAssertion { R.id.ed_edit_text.checkHasAnyTag("Halo") }
+    }
+
+    @Test
+    fun doesNotExists(){
+        val invalidId = -5
+        invalidId.checkDoesNotExist()
+    }
+
+    @Test
+    fun doesNotExistsFailure(){
+        verifyAssertion { R.id.tv_sample_text_view.checkDoesNotExist() }
+    }
+
+    @Test
+    fun hasBackgroundColorId(){
+        R.id.iv_image_view.checkHasBackgroundColor(R.color.colorPrimary)
+    }
+
+    @Test
+    fun hasBackgroundColorIdFailure(){
+        verifyAssertion { R.id.iv_image_view.checkHasBackgroundColor(R.color.colorPrimaryDark) }
+    }
+
+    @Test
+    fun hasBackgroundColorText(){
+        R.id.iv_image_view.checkHasBackgroundColor("#3F51B5")
+    }
+
+    @Test
+    fun hasBackgroundColorTextFailure(){
+        verifyAssertion { R.id.iv_image_view.checkHasBackgroundColor("#303F9F") }
+    }
 }
